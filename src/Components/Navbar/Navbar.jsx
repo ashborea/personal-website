99ea7104f41e8263adeedd7e9d66e8b8dcaa8359
Ash Borea <ashborea@gmail.com>
2024-02-05 20:12:27 +0100

    cleanup + create navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+        <div id="navbar">
+            <div className="page-logo">
+                <p>Marjorie <span className="green">Siad</span></p>
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Projets</li>
+                    <li>Contact</li>
+                    <li>Blog</li>
+                </ul>
+            </nav>
+        </div>
+    )
+
+}
+
+export default Navbar;
