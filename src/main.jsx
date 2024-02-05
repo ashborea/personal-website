20bf7f1ce07de7302c0a48495320716aeda2f75f
Ash Borea <ashborea@gmail.com>
2024-02-05 20:44:01 +0100

    adding tailwindcss <3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Navbar />
+  <div className="bg-slate-500 grid h-screen place-items-center">
+    <h1 className='text-3xl font-bold underline text-center'>Ce super design a été fait avec TAILWIND !!!!</h1>
+  </div>
   </React.StrictMode>,
 )
